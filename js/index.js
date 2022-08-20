5406c53d0754a59b8788a7d30853c0f3f01670c5
chenlianghong <clh021@gmail.com>
2022-08-20 23:36:02 +0800

    update config.yaml

--- js/index.js
+++ js/index.js
@@ -3,12 +3,11 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 
-try {
-    const doc = yaml.load(fs.readFileSync('./config.yaml', 'utf8'));
-    console.log(doc);
-} catch (e) {
-    console.log(e);
-}
+const config = yaml.load(fs.readFileSync('./config.yaml', 'utf8'));
+// console.log(JSON.stringify(config));
 
+config.calendar_list.forEach(cal => {
+    console.log(cal)
+});
 // const solar2lunarData = solarLunar.solar2lunar(2015, 10, 8); // 输入的日子为公历
 // const lunar2solarData = solarLunar.lunar2solar(2015, 8, 26); // 输入的日子为农历
