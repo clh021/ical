4a9946a8376a020f2d2818bb2990976cb91784df
chenlianghong <clh021@gmail.com>
2022-08-20 23:12:22 +0800

    add config.yaml

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-import solarLunar from 'solarLunar';
+// import solarLunar from 'solarLunar';
 
-const solar2lunarData = solarLunar.solar2lunar(2015, 10, 8); // 输入的日子为公历
-const lunar2solarData = solarLunar.lunar2solar(2015, 8, 26); // 输入的日子为农历
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+try {
+    const doc = yaml.load(fs.readFileSync('./config.yaml', 'utf8'));
+    console.log(doc);
+} catch (e) {
+    console.log(e);
+}
+
+// const solar2lunarData = solarLunar.solar2lunar(2015, 10, 8); // 输入的日子为公历
+// const lunar2solarData = solarLunar.lunar2solar(2015, 8, 26); // 输入的日子为农历
