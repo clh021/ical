7b8edbaac48a934ee15397f9287f114c2bef78ad
chenlianghong <clh021@gmail.com>
2022-08-21 14:06:51 +0800

    config.yaml update

--- js/index.js
+++ js/index.js
@@ -22,6 +22,7 @@ const getRealDate = (is_lunar, date_get) => {
 
 const confParse = (c) => {
     let year = (new Date()).getFullYear();
+
     // 今明年范围内的时间都在列表范围
     let tmpBegin = `${year}-${c.date_begin}`
     // let tmpEnd = `${year}-${c.date_end}`
@@ -35,27 +36,47 @@ const confParse = (c) => {
     //     // 根据配置决定是否转换农历
     //     c.date_next_end_real = getRealDate(c.is_lunar, c.date_next_end)
     // }
-    let tmpNext = c.date_next_begin;
+
+    // 两年内的事件列表
+    let tmpNext = new Date(tmpBegin);
     let event_list = [];
     do {
-        event_list.push(tmpNext);
-        // tmpNext
-    } while (c.date_begin.getFullYear < (year + 1));
+        let real = getRealDate(c.is_lunar, tmpNext)
+        event_list.push(new Date(real.toISOString()));
+        if (c.cycle.hasOwnProperty("year")) {
+            tmpNext.setFullYear(tmpNext.getFullYear() + c.cycle.year)
+        }
+        if (c.cycle.hasOwnProperty("month")) {
+            tmpNext.setMonth(tmpNext.getMonth() + c.cycle.month)
+        }
+        if (c.cycle.hasOwnProperty("day")) {
+            let hourCount = c.cycle.day * 24
+            tmpNext.setHours(tmpNext.getHours() + hourCount)
+        }
+    } while (tmpNext.getFullYear() < (year + 2));
+    c.event_list = event_list;
 
-
-    console.log(`${year}-${c.date_begin}`)
-    console.log(`${year}-${c.date_end}`)
+    // 两年内的提醒列表
+    c.alarm.forEach(a => {
+        let alarm_list = [];
+        event_list.forEach(e => {
+            let alarm_time = new Date(e)
+            if (a.hasOwnProperty("day")) {
+                let hourCount = a.day * 24
+                alarm_time.setHours(alarm_time.getHours() + hourCount)
+            }
+            alarm_list.push(alarm_time)
+        })
+        a.list = alarm_list
+    })
 }
 
-const configToTrigger = (c) => {
-    confParse(c)
-    console.log(c)
-    // 生成列表事件
-    // 排序
-    // 写入为 json 文件
-    // const solar2lunarData = solarLunar.solar2lunar(2015, 10, 8); // 输入的日子为公历
-    // const lunar2solarData = solarLunar.lunar2solar(2015, 8, 26); // 输入的日子为农历
-}
 
+// 读取配置 
 const config = yaml.load(fs.readFileSync('./config.yaml', 'utf8'));
-config.calendar_list.forEach(configToTrigger);
+// 解析配置
+config.calendar_list.forEach(confParse);
+// 生成列表事件
+// 排序
+// 写入结果
+fs.writeFileSync('./../dist/result.json', JSON.stringify(config))
